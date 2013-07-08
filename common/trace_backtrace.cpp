8bc25e44c59d6fb2554d39c069d17f79130b3435
José Fonseca <jfonseca@vmware.com>
2013-07-08 13:46:45 +0100

    Use consistent cpp logic for including backtrace.h
    __linux__ -> __ELF__

--- common/trace_backtrace.cpp
+++ common/trace_backtrace.cpp
@@ -112,7 +112,7 @@ bool backtrace_is_needed(const char* fname) {
 
 } /* namespace trace */
 
-#ifdef ANDROID
+#if defined(ANDROID)
 
 #include <dlfcn.h>
 #include "os.hpp"
@@ -270,7 +270,7 @@ std::vector<RawStackFrame> get_backtrace() {
 }
 
 /* end ANDROID */
-#elif defined __linux__
+#elif defined(__ELF__)
 
 #include <stdint.h>
 #include <dlfcn.h>
@@ -278,7 +278,7 @@ std::vector<RawStackFrame> get_backtrace() {
 #include <vector>
 #include <cxxabi.h>
 
-#include "backtrace.h"
+#include <backtrace.h>
 
 namespace trace {
 
