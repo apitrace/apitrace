3a5b5558016d693a9c70b230a30e35c57c9773cd
José Fonseca <jfonseca@vmware.com>
2014-05-02 22:50:21 +0100

    eglretrace: Support GL_OES_surfaceless_context by creating a dummy drawable.
    Should fix issue #248.

--- retrace/glretrace_egl.cpp
+++ retrace/glretrace_egl.cpp
@@ -62,6 +62,9 @@ static unsigned int current_api = EGL_OPENGL_ES_API;
  */
 static glws::Profile last_profile = glws::PROFILE_ES2;
 
+static glws::Drawable *null_drawable = NULL;
+
+
 static void
 createDrawable(unsigned long long orig_config, unsigned long long orig_surface);
 
@@ -221,8 +224,22 @@ static void retrace_eglDestroyContext(trace::Call &call) {
 }
 
 static void retrace_eglMakeCurrent(trace::Call &call) {
+    if (!call.ret->toSInt()) {
+        // Previously current rendering context and surfaces (if any) remain
+        // unchanged.
+        return;
+    }
+
     glws::Drawable *new_drawable = getDrawable(call.arg(1).toUIntPtr());
     Context *new_context = getContext(call.arg(3).toUIntPtr());
+
+    // Try to support GL_OES_surfaceless_context by creating a dummy drawable.
+    if (new_context && !new_drawable) {
+        if (!null_drawable) {
+            null_drawable = glretrace::createDrawable(last_profile);
+        }
+        new_drawable = null_drawable;
+    }
 
     glretrace::makeCurrent(call, new_drawable, new_context);
 }
