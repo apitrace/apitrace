apitrace/apitrace

commit 915d4dacfe286d2203c90983fca05c06b877e27c

d3dretrace: Replace casts with templates.
Smaller generated code, same type checking guarantees.

retrace/retrace.hpp
@@ -66,7 +66,7 @@ public:
      * Allocate an array with the same dimensions as the specified value.
      */
     inline void *
-    alloc(const trace::Value *value, size_t size) {
+    allocArray(const trace::Value *value, size_t size) {
         const trace::Array *array = value->toArray();
         if (array) {
             return ::ScopedAllocator::alloc(array->size() * size);
@@ -77,6 +77,12 @@ public:
         }
         assert(0);
         return NULL;
+    }
+
+    template< class T >
+    inline T *
+    allocArray(const trace::Value *value) {
+        return static_cast<T *>(allocArray(value, sizeof(T)));
     }
 
 };

retrace/retrace.py
@@ -69,10 +69,10 @@ class ValueAllocator(stdapi.Visitor):
         pass
 
     def visitArray(self, array, lvalue, rvalue):
-        print '    %s = static_cast<%s *>(_allocator.alloc(&%s, sizeof *%s));' % (lvalue, array.type, rvalue, lvalue)
+        print '    %s = _allocator.allocArray<%s>(&%s);' % (lvalue, array.type, rvalue)
 
     def visitPointer(self, pointer, lvalue, rvalue):
-        print '    %s = static_cast<%s *>(_allocator.alloc(&%s, sizeof *%s));' % (lvalue, pointer.type, rvalue, lvalue)
+        print '    %s = _allocator.allocArray<%s>(&%s);' % (lvalue, pointer.type, rvalue)
 
     def visitIntPointer(self, pointer, lvalue, rvalue):
         pass
@@ -154,7 +154,7 @@ class ValueDeserializer(stdapi.Visitor, stdapi.ExpanderMixin):
         print '    %s = static_cast<%s>((%s).toPointer());' % (lvalue, pointer, rvalue)
 
     def visitObjPointer(self, pointer, lvalue, rvalue):
-        print '    %s = static_cast<%s>(retrace::toObjPointer(call, %s));' % (lvalue, pointer, rvalue)
+        print '    %s = retrace::asObjPointer<%s>(call, %s);' % (lvalue, pointer.type, rvalue)
 
     def visitLinearPointer(self, pointer, lvalue, rvalue):
         print '    %s = static_cast<%s>(retrace::toPointer(%s));' % (lvalue, pointer, rvalue)
@@ -396,7 +396,7 @@ class Retracer:
 
     def deserializeThisPointer(self, interface):
         print r'    %s *_this;' % (interface.name,)
-        print r'    _this = static_cast<%s *>(retrace::toObjPointer(call, call.arg(0)));' % (interface.name,)
+        print r'    _this = retrace::asObjPointer<%s>(call, call.arg(0));' % (interface.name,)
         print r'    if (!_this) {'
         print r'        return;'
         print r'    }'

retrace/retrace_swizzle.hpp
@@ -120,7 +120,7 @@ toObjPointer(trace::Call &call, trace::Value &value);
 template< class T >
 inline T *
 asObjPointer(trace::Call &call, trace::Value &value) {
-    return reinterpret_cast<T *>(toObjPointer(call, value));
+    return static_cast<T *>(toObjPointer(call, value));
 }
 
 

commit 8f68e76f7d494e9a99a0dd0be961072e7b87d6f4

d3dretrace: Fix buffer overflow unserializng D3DPRESENT_PARAMETERS.
Regression introduced with 915d4dacfe286d2203c90983fca05c06b877e27c.

Second time the very same thing happens.  So this time try to make it
harder for future re-occurrences.

retrace/retrace.hpp
@@ -79,10 +79,18 @@ public:
         return NULL;
     }
 
+    /**
+     * XXX: We must not compute sizeof(T) inside the function body! d3d8.h and
+     * d3d9.h have declarations of D3DPRESENT_PARAMETERS and D3DVOLUME_DESC
+     * structures with different size sizes.  Multiple specializations of these
+     * will be produced (on debug builds, as on release builds the whole body
+     * is inlined.), and the linker will pick up one, leading to wrong results
+     * if the smallest specialization is picked.
+     */
     template< class T >
     inline T *
-    allocArray(const trace::Value *value) {
-        return static_cast<T *>(allocArray(value, sizeof(T)));
+    allocArray(const trace::Value *value, size_t sizeof_T = sizeof(T)) {
+        return static_cast<T *>(allocArray(value, sizeof_T));
     }
 
 };

retrace/scoped_allocator.hpp
@@ -63,10 +63,11 @@ public:
         return static_cast<void *>(&buf[1]);
     }
     
+    /* XXX: See comment in retrace::ScopedAllocator::allocArray template. */
     template< class T >
     inline T *
-    alloc(size_t size = 1) {
-        return static_cast<T *>(alloc(sizeof(T) * size));
+    alloc(size_t size = 1, size_t sizeof_T = sizeof(T)) {
+        return static_cast<T *>(alloc(sizeof_T * size));
     }
 
     /**