27eac49f8b2ce9a925487f4f2cfae03ac1110ce5
José Fonseca <jfonseca@vmware.com>
2014-09-24 14:46:58 +0100

    d3dretrace: Implement more internal DWM interfaces.

--- retrace/d3dretrace_dxgi_priv.cpp
+++ retrace/d3dretrace_dxgi_priv.cpp
@@ -57,7 +57,7 @@ GetInterface(IUnknown *pUnknown, void **ppvObj) {
 }
 
 
-class CDXGISwapChainDWM : public IDXGISwapChainDWM
+class CDXGISwapChainDWM : public IDXGISwapChainDWM1
 {
 protected:
     IDXGISwapChain *m_pSwapChain;
@@ -86,7 +86,8 @@ public:
     HRESULT STDMETHODCALLTYPE
     QueryInterface(REFIID riid, void **ppvObj)
     {
-        if (riid == IID_IDXGISwapChainDWM) {
+        if (riid == IID_IDXGISwapChainDWM ||
+            riid == IID_IDXGISwapChainDWM1) {
             return GetInterface(this, ppvObj);
         }
         return m_pSwapChain->QueryInterface(riid, ppvObj);
@@ -187,10 +188,214 @@ public:
     GetLastPresentCount(UINT *pLastPresentCount) {
         return m_pSwapChain->GetLastPresentCount(pLastPresentCount);
     }
+
+    /*
+     * IDXGISwapChainDWM1
+     */
+
+    HRESULT STDMETHODCALLTYPE
+    Present1(UINT SyncInterval, UINT Flags, UINT NumRects, const RECT *pRects, UINT NumScrollRects, const DXGI_SCROLL_RECT *pScrollRects) {
+        return m_pSwapChain->Present(SyncInterval, Flags);
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    GetLogicalSurfaceHandle(UINT64 *pHandle) {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    CheckDirectFlipSupport(UINT CheckDirectFlipFlags, IDXGIResource *pResource, BOOL *pSupported) {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    Present2(UINT SyncInterval, UINT Flags, UINT NumRects, const RECT *pRects, UINT NumScrollRects, const DXGI_SCROLL_RECT *pScrollRects, IDXGIResource *pResource) {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    GetCompositionSurface(HANDLE *pHandle) {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    GetFrameStatisticsDWM(DXGI_FRAME_STATISTICS_DWM *pStats) {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    GetMultiplaneOverlayCaps(DXGI_MULTIPLANE_OVERLAY_CAPS *pCaps) {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    CheckMultiplaneOverlaySupport(UINT Arg1, const DXGI_CHECK_MULTIPLANEOVERLAYSUPPORT_PLANE_INFO *pInfo, BOOL *pSupported) {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    PresentMultiplaneOverlay(UINT Arg1, UINT Arg2, UINT Arg3, const DXGI_PRESENT_MULTIPLANE_OVERLAY *pArg4) {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    CheckPresentDurationSupport(UINT Arg1, UINT *pArg2, UINT *pArg3) {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    SetPrivateFrameDuration(UINT Duration) {
+        return E_NOTIMPL;
+    }
 };
 
 
-class CDXGIFactoryDWM : public IDXGIFactoryDWM
+class CDXGIOutputDWM : public IDXGIOutputDWM
+{
+private:
+    IDXGIOutput *m_pOutput;
+
+    ~CDXGIOutputDWM() {
+    }
+
+public:
+    CDXGIOutputDWM(IDXGIOutput *pOutput) :
+        m_pOutput(pOutput)
+    {}
+
+    /*
+     * IUnknown
+     */
+
+    HRESULT STDMETHODCALLTYPE
+    QueryInterface(REFIID riid, void **ppvObj)
+    {
+        if (riid == IID_IDXGIOutputDWM) {
+            return GetInterface(this, ppvObj);
+        }
+        return m_pOutput->QueryInterface(riid, ppvObj);
+    }
+
+    ULONG STDMETHODCALLTYPE
+    AddRef(void) {
+        return m_pOutput->AddRef();
+    }
+
+    ULONG STDMETHODCALLTYPE
+    Release(void) {
+        ULONG cRef = m_pOutput->Release();
+        if (cRef == 0) {
+            delete this;
+        }
+        return cRef;
+    }
+
+    /*
+     * IDXGIOutputDWM
+     */
+
+    BOOL STDMETHODCALLTYPE
+    HasDDAClient(void) {
+        return FALSE;
+    }
+
+    void STDMETHODCALLTYPE
+    GetDesc(DXGI_OUTPUT_DWM_DESC *pDesc) {
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    FindClosestMatchingModeFromDesktop(const DXGI_MODE_DESC1 *pModeToMatch, DXGI_MODE_DESC1 *pClosestMatch, IUnknown *pConcernedDevice) {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    WaitForVBlankOrObjects(UINT NumHandles, const HANDLE *pHandles) {
+        return m_pOutput->WaitForVBlank();
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    GetFrameStatisticsDWM(DXGI_FRAME_STATISTICS_DWM *pStats) {
+        return E_NOTIMPL;
+    }
+
+};
+
+
+
+class CDXGIDeviceDWM : public IDXGIDeviceDWM
+{
+private:
+    IDXGIDevice *m_pDevice;
+
+    ~CDXGIDeviceDWM() {
+    }
+
+public:
+    CDXGIDeviceDWM(IDXGIDevice *pDevice) :
+        m_pDevice(pDevice)
+    {}
+
+    /*
+     * IUnknown
+     */
+
+    HRESULT STDMETHODCALLTYPE
+    QueryInterface(REFIID riid, void **ppvObj)
+    {
+        if (riid == IID_IDXGIDeviceDWM) {
+            return GetInterface(this, ppvObj);
+        }
+        return m_pDevice->QueryInterface(riid, ppvObj);
+    }
+
+    ULONG STDMETHODCALLTYPE
+    AddRef(void) {
+        return m_pDevice->AddRef();
+    }
+
+    ULONG STDMETHODCALLTYPE
+    Release(void) {
+        ULONG cRef = m_pDevice->Release();
+        if (cRef == 0) {
+            delete this;
+        }
+        return cRef;
+    }
+
+    /*
+     * IDXGIDeviceDWM
+     */
+
+    HRESULT STDMETHODCALLTYPE
+    OpenFenceByHandle(HANDLE hFence, DXGI_INTERNAL_TRACKED_FENCE **ppFence) {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    ReleaseResources(UINT NumResources, IDXGIResource * const *pResources) {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    CloseFence(DXGI_INTERNAL_TRACKED_FENCE *pFence) {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    PinResources(UINT NumResources, IDXGIResource * const *pResources) {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    UnpinResources(UINT NumResources, IDXGIResource * const *pResources) {
+        return E_NOTIMPL;
+    }
+};
+
+
+class CDXGIFactoryDWM :
+    public IDXGIFactoryDWM,
+    public IDXGIFactoryDWM2
 {
 private:
     IDXGIFactory *m_pFactory;
@@ -212,6 +417,9 @@ public:
     {
         if (riid == IID_IDXGIFactoryDWM) {
             return GetInterface(static_cast<IDXGIFactoryDWM *>(this), ppvObj);
+        }
+        if (riid == IID_IDXGIFactoryDWM2) {
+            return GetInterface(static_cast<IDXGIFactoryDWM2 *>(this), ppvObj);
         }
         return m_pFactory->QueryInterface(riid, ppvObj);
     }
@@ -250,6 +458,20 @@ public:
         }
         return hr;
     }
+
+    /*
+     * IDXGIFactoryDWM2
+     */
+
+    HRESULT STDMETHODCALLTYPE
+    CreateSwapChainDWM(IUnknown *pDevice, DXGI_SWAP_CHAIN_DESC *pDesc, IDXGIOutput *pOutput, IDXGISwapChainDWM **ppSwapChain) {
+        return CreateSwapChain(pDevice, pDesc, pOutput, ppSwapChain);
+    }
+
+    HRESULT STDMETHODCALLTYPE
+    CreateSwapChainDDA(IUnknown *pDevice, DXGI_SWAP_CHAIN_DESC1 *pDesc, IDXGIOutput *pOutput, IDXGISwapChainDWM1 **ppSwapChain) {
+        return E_NOTIMPL;
+    }
 };
 
 
@@ -268,6 +490,26 @@ overrideQueryInterface(IUnknown *pUnknown, REFIID riid, void **ppvObj, HRESULT *
         hr = pUnknown->QueryInterface(IID_IDXGIFactory, (VOID **)&pFactory);
         if (SUCCEEDED(hr)) {
             *ppvObj = new d3dretrace::CDXGIFactoryDWM(pFactory);
+            *pResult = S_OK;
+            return TRUE;
+        }
+    }
+
+    if (riid == IID_IDXGIOutputDWM) {
+        IDXGIOutput *pOutput = NULL;
+        hr = pUnknown->QueryInterface(IID_IDXGIOutput, (VOID **)&pOutput);
+        if (SUCCEEDED(hr)) {
+            *ppvObj = new d3dretrace::CDXGIOutputDWM(pOutput);
+            *pResult = S_OK;
+            return TRUE;
+        }
+    }
+
+    if (riid == IID_IDXGIDeviceDWM) {
+        IDXGIDevice *pDevice = NULL;
+        hr = pUnknown->QueryInterface(IID_IDXGIDevice, (VOID **)&pDevice);
+        if (SUCCEEDED(hr)) {
+            *ppvObj = new d3dretrace::CDXGIDeviceDWM(pDevice);
             *pResult = S_OK;
             return TRUE;
         }
