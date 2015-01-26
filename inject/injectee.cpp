ae318af2e5a3d08af93f1e808a9829dacd09eae2
José Fonseca <jfonseca@vmware.com>
2015-01-26 14:47:57 +0000

    inject: Fix injection of Direct2D wrapper.

--- inject/injectee.cpp
+++ inject/injectee.cpp
@@ -947,6 +947,9 @@ DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
             registerModuleHooks("d3d10.dll",   g_hHookModule);
             registerModuleHooks("d3d10_1.dll", g_hHookModule);
             registerModuleHooks("d3d11.dll",   g_hHookModule);
+        } else if (stricmp(szNewDllBaseName, "d2d1trace.dll") == 0) {
+            registerModuleHooks("d2d1.dll",    g_hHookModule);
+            registerModuleHooks("dwrite.dll",  g_hHookModule);
         } else {
             registerModuleHooks(szNewDllBaseName, g_hHookModule);
         }
