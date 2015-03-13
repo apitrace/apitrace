cc41a48baff8d13bcd10cd96879ffdcf29beb1f2
Jose Fonseca <jfonseca@vmware.com>
2015-03-13 10:35:27 +0000

    cmake: Use FindPackageHandleStandardArgs for finding procps.

--- cmake/Findprocps.cmake
+++ cmake/Findprocps.cmake
@@ -2,22 +2,15 @@
 #
 # Once done this will define:
 #
-#  procps_FOUND
-#  procps_INCLUDE_DIR
-#  procps_LIBRARY
+#  PROCPS_FOUND
+#  PROCPS_INCLUDE_DIR
+#  PROCPS_LIBRARY
 #
 
-include (FindPackageMessage)
+include (FindPackageHandleStandardArgs)
 
 if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
-    find_path (procps_INCLUDE_DIR proc/readproc.h)
-    find_library (procps_LIBRARY NAMES proc procps)
-    if (procps_INCLUDE_DIR AND procps_LIBRARY)
-        set (procps_FOUND 1)
-        mark_as_advanced (procps_FOUND)
-        find_package_message (procps "Found procps" "[${procps_LIBRARY}][${procps_INCLUDE_DIR}]")
-    else ()
-        find_package_message (procps "procps not found" "[${procps_LIBRARY}][${procps_INCLUDE_DIR}]")
-    endif ()
+    find_path (PROCPS_INCLUDE_DIR proc/readproc.h)
+    find_library (PROCPS_LIBRARY NAMES proc procps)
+    find_package_handle_standard_args (procps DEFAULT_MSG PROCPS_LIBRARY PROCPS_INCLUDE_DIR)
 endif()
-
