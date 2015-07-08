a01a4b5ad7dfddd26deb1b93d9dffc684ba52ba7
Jose Fonseca <jfonseca@vmware.com>
2015-07-08 17:00:43 +0100

    d3dtrace: Ensure we fallback to height*pitch for unknown/unexpected formats.

--- helpers/d3d8size.hpp
+++ helpers/d3d8size.hpp
@@ -127,6 +127,8 @@ _getFormatSize(D3DFORMAT Format, size_t & BlockSize, UINT & BlockWidth, UINT & B
 
     default:
         os::log("apitrace: warning: %s: unknown D3DFMT %u\n", __FUNCTION__, Format);
+        BlockWidth = 0;
+        BlockHeight = 0;
         break;
     }
 }
