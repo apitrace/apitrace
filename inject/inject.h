apitrace/apitrace

commit 6ddfd98cdafefaa1b60273d5568b8dbd13730dae

inject: Detect mixed architectures for child processes too.

inject/inject.h
@@ -203,6 +203,37 @@ CloseSharedMem(void) {
 }
 
 
+/*
+ * XXX: Mixed architecture don't quite work.  See also
+ * http://www.corsix.org/content/dll-injection-and-wow64
+ */
+static BOOL
+isDifferentArch(HANDLE hProcess)
+{
+    typedef BOOL (WINAPI *PFNISWOW64PROCESS)(HANDLE, PBOOL);
+    PFNISWOW64PROCESS pfnIsWow64Process;
+    pfnIsWow64Process = (PFNISWOW64PROCESS)
+        GetProcAddress(GetModuleHandleA("kernel32"), "IsWow64Process");
+    if (!pfnIsWow64Process) {
+        return FALSE;
+    }
+
+#ifdef _WIN64
+    BOOL isThisWow64 = FALSE;
+#else
+    BOOL isThisWow64 = TRUE;
+#endif
+
+    BOOL isOtherWow64 = FALSE;
+    if (!pfnIsWow64Process(hProcess, &isOtherWow64)) {
+        logLastError("IsWow64Process failed");
+        return FALSE;
+    }
+
+    return bool(isThisWow64) != bool(isOtherWow64);
+}
+
+
 static BOOL
 injectDll(HANDLE hProcess, const char *szDllPath)
 {

inject/injectee.cpp
@@ -118,16 +118,23 @@ MyCreateProcessCommon(BOOL bRet,
                       LPPROCESS_INFORMATION lpProcessInformation)
 {
     if (!bRet) {
+        debugPrintf("inject: warning: failed to create child process\n");
         return;
     }
 
     DWORD dwLastError = GetLastError();
 
-    char szDllPath[MAX_PATH];
-    GetModuleFileNameA(g_hThisModule, szDllPath, sizeof szDllPath);
+    if (isDifferentArch(lpProcessInformation->hProcess)) {
+        debugPrintf("inject: error: child process %lu has different architecture\n",
+                    GetProcessId(lpProcessInformation->hProcess));
+    } else {
+        char szDllPath[MAX_PATH];
+        GetModuleFileNameA(g_hThisModule, szDllPath, sizeof szDllPath);
 
-    if (!injectDll(lpProcessInformation->hProcess, szDllPath)) {
-        debugPrintf("inject: warning: failed to inject child process\n");
+        if (!injectDll(lpProcessInformation->hProcess, szDllPath)) {
+            debugPrintf("inject: warning: failed to inject into child process %lu\n",
+                        GetProcessId(lpProcessInformation->hProcess));
+        }
     }
 
     if (!(dwCreationFlags & CREATE_SUSPENDED)) {

inject/injector.cpp
@@ -683,32 +683,16 @@ main(int argc, char *argv[])
         hProcess = processInfo.hProcess;
     }
 
-    /*
-     * XXX: Mixed architecture don't quite work.  See also
-     * http://www.corsix.org/content/dll-injection-and-wow64
-     */
-    {
-        typedef BOOL (WINAPI *PFNISWOW64PROCESS)(HANDLE, PBOOL);
-        PFNISWOW64PROCESS pfnIsWow64Process;
-        pfnIsWow64Process = (PFNISWOW64PROCESS)
-            GetProcAddress(GetModuleHandleA("kernel32"), "IsWow64Process");
-        if (pfnIsWow64Process) {
-            BOOL isParentWow64 = FALSE;
-            BOOL isChildWow64 = FALSE;
-            if (pfnIsWow64Process(GetCurrentProcess(), &isParentWow64) &&
-                pfnIsWow64Process(hProcess, &isChildWow64) &&
-                isParentWow64 != isChildWow64) {
-                debugPrintf("error: binaries mismatch: you need to use the "
+    if (isDifferentArch(hProcess)) {
+        debugPrintf("error: binaries mismatch: you need to use the "
 #ifdef _WIN64
-                        "32-bits"
+                "32-bits"
 #else
-                        "64-bits"
+                "64-bits"
 #endif
-                        " apitrace binaries to trace this application\n");
-                TerminateProcess(hProcess, 1);
-                return 1;
-            }
-        }
+                " apitrace binaries to trace this application\n");
+        TerminateProcess(hProcess, 1);
+        return 1;
     }
 
     if (bAttachDwm && IsWindows8OrGreater()) {

commit bafc84295df205d0db53353734ec6d61e0f180c5

inject: Take in account that IsWow64Process returns false on 32-bits Windows.
Regression introduced by 6ddfd98cdafefaa1b60273d5568b8dbd13730dae.

Fixes https://github.com/apitrace/apitrace/issues/378

inject/inject.h
@@ -218,14 +218,11 @@ isDifferentArch(HANDLE hProcess)
         return FALSE;
     }
 
-#ifdef _WIN64
-    BOOL isThisWow64 = FALSE;
-#else
-    BOOL isThisWow64 = TRUE;
-#endif
-
-    BOOL isOtherWow64 = FALSE;
-    if (!pfnIsWow64Process(hProcess, &isOtherWow64)) {
+    // NOTE: IsWow64Process will return false on 32-bits Windows
+    BOOL isThisWow64;
+    BOOL isOtherWow64;
+    if (!pfnIsWow64Process(GetCurrentProcess(), &isThisWow64) ||
+        !pfnIsWow64Process(hProcess, &isOtherWow64)) {
         logLastError("IsWow64Process failed");
         return FALSE;
     }