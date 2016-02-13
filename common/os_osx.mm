415d0fd9420c698c4d85c946ef8f63c6b8e9bbd4
Rolan Reznik <rolan.reznik@icloud.com>
2016-02-13 23:42:48 +0200

    getTemporaryDirectoryPath memleack fix.

--- common/os_osx.mm
+++ common/os_osx.mm
@@ -7,12 +7,15 @@ namespace os {
 
 String
 getTemporaryDirectoryPath(void) {
+    String result("/tmp"); 
     NSString *tempDir = NSTemporaryDirectory();
     const size_t len = strlen([tempDir fileSystemRepresentation]);
-    char *string = (char *)calloc((sizeof(char) * len) + 1, 1);
+    char *string = (char *)calloc(sizeof(char) * (len + 1), 1);
     if (![tempDir getFileSystemRepresentation:string maxLength:len + 1]) {
         NSLog(@"apitrace: error: cant get temporary directory");
     }
-    return String(string);
+    result = String(string);
+    free(string);
+    return result;
 }
 }
