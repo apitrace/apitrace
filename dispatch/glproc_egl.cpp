372933b1b8f971adfc5ec6fbd738ff6f259f4a5d
Adrian Negreanu <adrian.negreanu@nxp.com>
2018-02-26 12:16:50 +0200

    dispatch: check if RTLD_DEEPBIND exists
    Signed-off-by: Adrian Negreanu <adrian.negreanu@nxp.com>

--- dispatch/glproc_egl.cpp
+++ dispatch/glproc_egl.cpp
@@ -89,7 +89,11 @@ _getPublicProcAddress(const char *procName)
     if (procName[0] == 'e' && procName[1] == 'g' && procName[2] == 'l') {
         static void *libEGL = NULL;
         if (!libEGL) {
-            libEGL = _dlopen("libEGL.so", RTLD_LOCAL | RTLD_LAZY | RTLD_DEEPBIND);
+            libEGL = _dlopen("libEGL.so", RTLD_LOCAL | RTLD_LAZY
+#ifdef APITRACE_HAS_RTLD_DEEPBIND
+                    | RTLD_DEEPBIND
+#endif
+                    );
             if (!libEGL) {
                 return NULL;
             }
@@ -132,7 +136,11 @@ _getPublicProcAddress(const char *procName)
 
         static void *libGLESv2 = NULL;
         if (!libGLESv2) {
-            libGLESv2 = _dlopen("libGLESv2.so", RTLD_LOCAL | RTLD_LAZY | RTLD_DEEPBIND);
+            libGLESv2 = _dlopen("libGLESv2.so", RTLD_LOCAL | RTLD_LAZY
+#ifdef APITRACE_HAS_RTLD_DEEPBIND
+                    | RTLD_DEEPBIND
+#endif
+                    );
         }
         if (libGLESv2) {
             proc = dlsym(libGLESv2, procName);
@@ -143,7 +151,11 @@ _getPublicProcAddress(const char *procName)
 
         static void *libGLESv1 = NULL;
         if (!libGLESv1) {
-            libGLESv1 = _dlopen("libGLESv1_CM.so", RTLD_LOCAL | RTLD_LAZY | RTLD_DEEPBIND);
+            libGLESv1 = _dlopen("libGLESv1_CM.so", RTLD_LOCAL | RTLD_LAZY
+#ifdef APITRACE_HAS_RTLD_DEEPBIND
+                    | RTLD_DEEPBIND
+#endif
+                    );
         }
         if (libGLESv1) {
             proc = dlsym(libGLESv1, procName);
