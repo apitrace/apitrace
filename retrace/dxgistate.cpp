698dc9095f380fe68b244e6a4a417fbccf2556ac
Jose Fonseca <jfonseca@vmware.com>
2018-12-06 10:18:58 +0000

    d3dretrace: Don't convert from sRGB to linear colorspace when dumping sRGB surfaces.
    This makes screenshots look much darker than what's presented on screen,
    and loses a lot of signal for comparison.
    
    Note however that any reference images for D3D10/D3D11 traces that use
    DXGI_FORMAT_*_SRGB will have to be regenerated.

--- retrace/dxgistate.cpp
+++ retrace/dxgistate.cpp
@@ -100,7 +100,7 @@ ChooseConversionFormat(DXGI_FORMAT Format, unsigned &numChannels, image::Channel
     case DXGI_FORMAT_B8G8R8A8_UNORM_SRGB:
     case DXGI_FORMAT_B8G8R8X8_UNORM_SRGB:
     case DXGI_FORMAT_BC7_UNORM_SRGB:
-        return DXGI_FORMAT_R8G8B8A8_UNORM;
+        return DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
 
     // Signed normalized
     case DXGI_FORMAT_R16G16B16A16_SNORM:
