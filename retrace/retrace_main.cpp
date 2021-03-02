apitrace/apitrace

commit 3d7d6e16606542e3255e03d8e99335aece7e5cdf

retrace: take end of frame shapshots always from back buffer
When specifying "--snapshot=frame" it can be assumed that the user
is interested in the renderiung output that goes to the screen and
not to some framebuffer attachment that happens to be bound to be
bound to an active framebuffer and set to be the drawbuffer.

Consequently, override the frame capture logic in case the frame-end
call triggers the snapshot to get the backbuffer contents.

Closes: #699

v2: - rename endOfFrame to backBuffer (Jose)
    - add parameter to D3D replay (Jose)

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

retrace/retrace_main.cpp
@@ -130,7 +130,7 @@ long long perFrameDelayUsec = 0;
 long long minFrameDurationUsec = 0;
 
 static void
-takeSnapshot(unsigned call_no);
+takeSnapshot(unsigned call_no, bool backBuffer);
 
 
 /**
@@ -165,7 +165,7 @@ frameComplete(trace::Call &call)
     }
 
     if (snapshotFrequency.contains(call)) {
-        takeSnapshot(call.no);
+        takeSnapshot(call.no, true);
         if (call.no >= snapshotFrequency.getLast()) {
             exit(0);
         }
@@ -186,7 +186,7 @@ public:
     }
 
     image::Image *
-    getSnapshot(int n) override {
+    getSnapshot(int n, bool backBuffer) override {
         return NULL;
     }
 
@@ -218,12 +218,12 @@ static Snapshotter *snapshotter;
  * Take snapshots.
  */
 static void
-takeSnapshot(unsigned call_no, int mrt, unsigned snapshot_no) {
+takeSnapshot(unsigned call_no, int mrt, unsigned snapshot_no, bool backBuffer) {
 
     assert(dumpingSnapshots);
     assert(snapshotPrefix);
 
-    std::unique_ptr<image::Image> src(dumper->getSnapshot(mrt));
+    std::unique_ptr<image::Image> src(dumper->getSnapshot(mrt, backBuffer));
     if (!src) {
         /* TODO for mrt>0 we probably don't want to treat this as an error: */
         if (mrt == 0)
@@ -279,7 +279,7 @@ takeSnapshot(unsigned call_no, int mrt, unsigned snapshot_no) {
 }
 
 static void
-takeSnapshot(unsigned call_no)
+takeSnapshot(unsigned call_no, bool backBuffer)
 {
     static signed long long last_call_no = -1;
     if (call_no == last_call_no) {
@@ -292,10 +292,10 @@ takeSnapshot(unsigned call_no)
 
     if (retrace::snapshotMRT) {
         for (int mrt = -2; mrt < cnt; mrt++) {
-            takeSnapshot(call_no, mrt, snapshot_no);
+            takeSnapshot(call_no, mrt, snapshot_no, backBuffer);
         }
     } else {
-        takeSnapshot(call_no, 0, snapshot_no);
+        takeSnapshot(call_no, 0, snapshot_no, backBuffer);
     }
 
     snapshot_no++;
@@ -319,7 +319,7 @@ retraceCall(trace::Call *call) {
     retracer.retrace(*call);
 
     if (snapshotFrequency.contains(*call)) {
-        takeSnapshot(call->no);
+        takeSnapshot(call->no, false);
         if (call->no >= snapshotFrequency.getLast()) {
             exit(0);
         }

retrace/d3dretrace.hpp
@@ -56,7 +56,7 @@ public:
     }
 
     image::Image *
-    getSnapshot(int n) {
+    getSnapshot(int n, bool backBuffer) {
         if ((n != 0) || !pLastDevice) {
             return NULL;
         }

retrace/glretrace_main.cpp
@@ -815,11 +815,11 @@ public:
     }
 
     image::Image *
-    getSnapshot(int n) override {
+    getSnapshot(int n, bool backBuffer) override {
         if (!glretrace::getCurrentContext()) {
             return NULL;
         }
-        return glstate::getDrawBufferImage(n);
+        return glstate::getDrawBufferImage(n, backBuffer);
     }
 
     bool

retrace/glstate.hpp
@@ -68,7 +68,7 @@ int
 getDrawBufferImageCount(void);
 
 image::Image *
-getDrawBufferImage(int n);
+getDrawBufferImage(int n, bool backBuffer);
 
 
 } /* namespace glstate */

retrace/glstate_images.cpp
@@ -1242,7 +1242,7 @@ getDrawBufferImageCount()
 
 
 image::Image *
-getDrawBufferImage(int n)
+getDrawBufferImage(int n, bool backBuffer)
 {
     Context context;
 
@@ -1265,7 +1265,7 @@ getDrawBufferImage(int n)
         framebuffer_target = GL_FRAMEBUFFER;
     }
     GLint draw_framebuffer = 0;
-    if (context.framebuffer_object) {
+    if (context.framebuffer_object && !backBuffer) {
         glGetIntegerv(framebuffer_binding, &draw_framebuffer);
     }
 
@@ -1289,7 +1289,7 @@ getDrawBufferImage(int n)
 
     GLint draw_buffer = GL_NONE;
     ImageDesc desc;
-    if (draw_framebuffer) {
+    if (draw_framebuffer && !backBuffer) {
         if (context.ARB_draw_buffers) {
             glGetIntegerv(GL_DRAW_BUFFER0 + n, &draw_buffer);
             if (draw_buffer == GL_NONE) {
@@ -1304,7 +1304,7 @@ getDrawBufferImage(int n)
             return NULL;
         }
     } else if (n == 0) {
-        if (context.ES) {
+        if (context.ES || backBuffer) {
             // XXX: Draw buffer is always FRONT for single buffer context, BACK
             // for double buffered contexts. There is no way to know which (as
             // GL_DOUBLEBUFFER state is also unavailable), so always assume

retrace/retrace.hpp
@@ -251,7 +251,7 @@ public:
     getSnapshotCount(void) = 0;
 
     virtual image::Image *
-    getSnapshot(int n) = 0;
+    getSnapshot(int n, bool backBuffer) = 0;
 
     virtual bool
     canDump(void) = 0;

commit e33143608e4a694a05f62499c5e35fa669726021

retrace: Add an option to force using the back buffer as snapshot source
Defaulting to reading from the back buffer when snapshots are taken at
frame frequency broke some test that only use FBOs and not always
clear the back buffer properly so that garbage was read back.
Add an option that the caller can force reading from the backbuffer,
but default to the old behaviour of reading back from the currently
bound draw buffer.

Fixes: 3d7d6e16606542e3255e03d8e99335aece7e5cdf
  retrace: take end of frame shapshots always from back buffer

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

retrace/retrace_main.cpp
@@ -120,6 +120,7 @@ bool useCallNos = true;
 bool singleThread = false;
 bool ignoreRetvals = false;
 bool contextCheck = true;
+bool snapshotForceBackbuffer = false;
 int64_t minCpuTime = 1000;
 
 unsigned frameNo = 0;
@@ -165,7 +166,7 @@ frameComplete(trace::Call &call)
     }
 
     if (snapshotFrequency.contains(call)) {
-        takeSnapshot(call.no, true);
+        takeSnapshot(call.no, snapshotForceBackbuffer);
         if (call.no >= snapshotFrequency.getLast()) {
             exit(0);
         }
@@ -319,7 +320,7 @@ retraceCall(trace::Call *call) {
     retracer.retrace(*call);
 
     if (snapshotFrequency.contains(*call)) {
-        takeSnapshot(call->no, false);
+        takeSnapshot(call->no, snapshotForceBackbuffer);
         if (call->no >= snapshotFrequency.getLast()) {
             exit(0);
         }
@@ -711,6 +712,7 @@ usage(const char *argv0) {
         "  -S, --snapshot=CALLSET  calls to snapshot (default is every frame)\n"
         "      --snapshot-interval=N    specify a frame interval when generating snaphots (default is 0)\n"
         "  -t, --snapshot-threaded encode screenshots on multiple threads\n"
+        "      --snapshot-force-backbuffer always read from the backbuffer when taking a snapshot (default read from the current draw buffer)\n"
         "  -v, --verbose           increase output verbosity\n"
         "  -D, --dump-state=CALL   dump state at specific call no\n"
         "      --dump-format=FORMAT dump state format (`json` or `ubjson`)\n"
@@ -755,6 +757,7 @@ enum {
     SNAPSHOT_ALPHA_OPT,
     SNAPSHOT_FORMAT_OPT,
     SNAPSHOT_INTERVAL_OPT,
+    SNAPSHOT_FORCE_BACKBUFFER_OPT,
     DUMP_FORMAT_OPT,
     MARKERS_OPT,
     MIN_CPU_TIME_OPT,
@@ -796,6 +799,7 @@ longOptions[] = {
     {"snapshot-alpha", no_argument, 0, SNAPSHOT_ALPHA_OPT},
     {"snapshot-format", required_argument, 0, SNAPSHOT_FORMAT_OPT},
     {"snapshot-interval", required_argument, 0, SNAPSHOT_INTERVAL_OPT},
+    {"snapshot-force-backbuffer", no_argument, 0, SNAPSHOT_FORCE_BACKBUFFER_OPT},
     {"snapshot-prefix", required_argument, 0, 's'},
     {"snapshot-threaded", no_argument, 0, 't'},
     {"verbose", no_argument, 0, 'v'},
@@ -1145,6 +1149,9 @@ int main(int argc, char **argv)
             break;
         case SNAPSHOT_INTERVAL_OPT:
             snapshotInterval = atoi(optarg);
+            break;
+        case SNAPSHOT_FORCE_BACKBUFFER_OPT:
+            snapshotForceBackbuffer = true;
             break;
         case 't':
             snapshotThreaded = true;