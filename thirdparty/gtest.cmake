91fe111d3343d01394b7377c742b1a732bf013a2
Jose Fonseca <jfonseca@vmware.com>
2021-04-17 23:35:27 +0100

    gtest: Reimport as a git submodule.

--- thirdparty/gtest.cmake
+++ thirdparty/gtest.cmake
@@ -1,10 +1,12 @@
-add_library (gtest gtest/src/gtest-all.cc)
+add_library (gtest
+    gtest/googletest/src/gtest-all.cc
+)
 
 target_compile_definitions (gtest PUBLIC GTEST_HAS_TR1_TUPLE=0)
 
 target_include_directories (gtest
-    PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/gtest/include
-    PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/gtest
+    PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/gtest/googletest/include
+    PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/gtest/googletest
 )
 
 target_link_libraries (gtest PUBLIC ${CMAKE_THREAD_LIBS_INIT})
