apitrace/apitrace

commit 93ac817ac9e6b21bc02753f1ee0c82c9d979d53e

d3dretrace: Partial support for choosing discrete/integrated GPU.

retrace/retrace_main.cpp
@@ -659,7 +659,7 @@ usage(const char *argv0) {
         "      --core              use core profile\n"
         "      --db                use a double buffer visual (default)\n"
         "      --samples=N         use GL_ARB_multisample (default is 1)\n"
-        "      --driver=DRIVER     force driver type (`hw`, `sw`, `ref`, `null`, or driver module name)\n"
+        "      --driver=DRIVER     force driver type (`hw`, `dgpu`, `igpu`, `sw`, `ref`, `null`, or driver module name)\n"
         "      --fullscreen        allow fullscreen\n"
         "      --headless          don't show windows\n"
         "      --sb                use a single buffer visual\n"
@@ -933,6 +933,22 @@ VectoredHandler(PEXCEPTION_POINTERS pExceptionInfo)
 #endif  // _WIN32
 
 
+/*
+ * Direct NVIDIA Optimus driver to use the High Performance Graphics.
+ *
+ * If we invoked glGetString(GL_VENDOR) or glGetString(GL_RENDERER) this
+ * wouldn't be necessary, but glretrace skips such calls.
+ *
+ * See also:
+ * - http://developer.download.nvidia.com/devzone/devcenter/gamegraphics/files/OptimusRenderingPolicies.pdf
+ */
+#ifdef _WIN32
+extern "C" {
+     __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000000;
+}
+#endif
+
+
 extern "C"
 int main(int argc, char **argv)
 {
@@ -1005,6 +1021,15 @@ int main(int argc, char **argv)
         case DRIVER_OPT:
             if (strcasecmp(optarg, "hw") == 0) {
                 driver = DRIVER_HARDWARE;
+            } else if (strcasecmp(optarg, "dgpu") == 0) {
+                driver = DRIVER_DISCRETE;
+#ifdef _WIN32
+                NvOptimusEnablement = 0x00000001;
+#endif
+            } else if (strcasecmp(optarg, "igpu") == 0) {
+                driver = DRIVER_INTEGRATED;
+            } else if (strcasecmp(optarg, "sw") == 0) {
+                driver = DRIVER_SOFTWARE;
             } else if (strcasecmp(optarg, "sw") == 0) {
                 driver = DRIVER_SOFTWARE;
             } else if (strcasecmp(optarg, "ref") == 0) {
@@ -1243,19 +1268,3 @@ int main(int argc, char **argv)
 
     return 0;
 }
-
-
-/*
- * Direct NVIDIA Optimus driver to use the High Performance Graphics.
- *
- * If we invoked glGetString(GL_VENDOR) or glGetString(GL_RENDERER) this
- * wouldn't be necessary, but glretrace skips such calls.
- *
- * See also:
- * - http://developer.download.nvidia.com/devzone/devcenter/gamegraphics/files/OptimusRenderingPolicies.pdf
- */
-#ifdef _WIN32
-extern "C" {
-     __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
-}
-#endif

retrace/d3d9retrace.py
@@ -43,26 +43,67 @@ class D3DRetracer(Retracer):
         Retracer.retraceApi(self, api)
 
     def invokeFunction(self, function):
-        if function.name in ('Direct3DCreate9', 'Direct3DCreate9Ex'):
-            print 'if (retrace::debug >= 3 && !g_szD3D9DllName && LoadLibraryA("d3d9d.dll")) {'
-            print '    /*'
-            print '     * D3D9D only works for simple applications, it will often report bogus errors'
-            print '     * on complex traces, or traces which use unofficial D3D9 features.'
-            print '     */'
-            print '    g_szD3D9DllName = "d3d9d.dll";'
-            print '    SDKVersion |= 0x80000000;'
-            print '} else {'
-            print '    SDKVersion &= ~0x80000000;'
-            print '}'
+        if function.name.startswith('Direct3DCreate9'):
+            print r'    if (retrace::debug >= 3 && !g_szD3D9DllName && LoadLibraryA("d3d9d.dll")) {'
+            print r'        /*'
+            print r'         * D3D9D only works for simple applications, it will often report bogus errors'
+            print r'         * on complex traces, or traces which use unofficial D3D9 features.'
+            print r'         */'
+            print r'        g_szD3D9DllName = "d3d9d.dll";'
+            print r'        SDKVersion |= 0x80000000;'
+            print r'    } else {'
+            print r'        SDKVersion &= ~0x80000000;'
+            print r'    }'
 
         # d3d8d.dll can be found in the Aug 2007 DXSDK.  It works on XP, but
         # not on Windows 7.
-        if function.name in ('Direct3DCreate8'):
-            print 'if (retrace::debug >= 3 && !g_szD3D8DllName && LoadLibraryA("d3d8d.dll")) {'
-            print '    g_szD3D8DllName = "d3d8d.dll";'
-            print '}'
+        if function.name.startswith('Direct3DCreate8'):
+            print r'    if (retrace::debug >= 3 && !g_szD3D8DllName && LoadLibraryA("d3d8d.dll")) {'
+            print r'        g_szD3D8DllName = "d3d8d.dll";'
+            print r'    }'
+
+        if function.name.startswith('Direct3DCreate9'):
+            print r'    // 0: default'
+            print r'    // 1: force discrete'
+            print r'    // 2/3: force integrated'
+            print r'    UINT uHybrid = 0;'
+            print r'    if (retrace::driver == retrace::DRIVER_DISCRETE) {'
+            print r'        uHybrid = 1;'
+            print r'    }'
+            print r'    if (retrace::driver == retrace::DRIVER_INTEGRATED) {'
+            print r'        uHybrid = 2;'
+            print r'    }'
+            print r'    if (uHybrid != 0) {'
+            print r'        HMODULE hD3D9 = LoadLibraryA("D3D9");'
+            print r'        assert(hD3D9);'
+            print r'        typedef void (WINAPI *PFNDIRECT3D9FORCEHYBRIDENUMERATION)(UINT);'
+            print r'        PFNDIRECT3D9FORCEHYBRIDENUMERATION pfnDirect3D9ForceHybridEnumeration ='
+            print r'            (PFNDIRECT3D9FORCEHYBRIDENUMERATION)GetProcAddress(hD3D9, MAKEINTRESOURCEA(16));'
+            print r'        if (pfnDirect3D9ForceHybridEnumeration) {'
+            print r'            pfnDirect3D9ForceHybridEnumeration(uHybrid);'
+            print r'        }'
+            print r'    }'
 
         Retracer.invokeFunction(self, function)
+
+        if function.name.startswith('Direct3DCreate'):
+            print r'    if (retrace::driver == retrace::DRIVER_DISCRETE ||'
+            print r'        retrace::driver == retrace::DRIVER_INTEGRATED) {'
+            if function.name == 'Direct3DCreate9Ex':
+                print r'        auto pD3D = SUCCEEDED(_result) ? *ppD3D : nullptr;'
+            else:
+                print r'        auto pD3D = _result;'
+            if function.name.startswith('Direct3DCreate9'):
+                print r'        D3DADAPTER_IDENTIFIER9 Identifier;'
+            else:
+                assert function.name.startswith('Direct3DCreate8')
+                print r'        D3DADAPTER_IDENTIFIER8 Identifier;'
+            print r'        if (pD3D) {'
+            print r'            if (SUCCEEDED(pD3D->GetAdapterIdentifier(D3DADAPTER_DEFAULT, 0, &Identifier))) {'
+            print r'                std::cerr << "info: using " << Identifier.Description << std::endl;'
+            print r'            }'
+            print r'        }'
+            print r'    }'
 
     createDeviceMethodNames = [
         'CreateDevice',
@@ -139,11 +180,13 @@ class D3DRetracer(Retracer):
                 print r'            pPresentationParameters->BackBufferFormat = Mode.Format;'
                 print r'        }'
                 print r'    }'
-        
+
         if method.name in self.createDeviceMethodNames:
             # override the device type
             print r'    switch (retrace::driver) {'
             print r'    case retrace::DRIVER_HARDWARE:'
+            print r'    case retrace::DRIVER_DISCRETE:'
+            print r'    case retrace::DRIVER_INTEGRATED:'
             print r'        DeviceType = D3DDEVTYPE_HAL;'
             print r'        break;'
             print r'    case retrace::DRIVER_SOFTWARE:'

retrace/dxgiretrace.py
@@ -140,6 +140,9 @@ class D3DRetracer(Retracer):
         # need to override inside the EnumAdapters call below
         print r'    if (pAdapter == NULL) {'
         print r'        switch (retrace::driver) {'
+        print r'        case retrace::DRIVER_INTEGRATED:'
+        print r'            retrace::warning(call) << "integrated gpu selection not yet implemented\n";'
+        print r'        case retrace::DRIVER_DISCRETE:'
         print r'        case retrace::DRIVER_HARDWARE:'
         print r'            DriverType = %s_HARDWARE;' % enum
         print r'            Software = NULL;'

retrace/retrace.hpp
@@ -173,6 +173,8 @@ extern bool dumpingSnapshots;
 enum Driver {
     DRIVER_DEFAULT,
     DRIVER_HARDWARE, // force hardware
+    DRIVER_DISCRETE, // force discrete GPU
+    DRIVER_INTEGRATED, // force integrated GPU
     DRIVER_SOFTWARE,
     DRIVER_REFERENCE,
     DRIVER_NULL,

commit 6093e60ccf6eb03915da31ff95119eb8e804a72d

retrace: remove redundant comparison for the --driver option values
Found upon inspection.

Fixes: 93ac817a ("d3dretrace: Partial support for choosing discrete/integrated GPU.")

Signed-off-by: Andres Gomez <agomez@igalia.com>

retrace/retrace_main.cpp
@@ -1101,8 +1101,6 @@ int main(int argc, char **argv)
                 driver = DRIVER_INTEGRATED;
             } else if (strcasecmp(optarg, "sw") == 0) {
                 driver = DRIVER_SOFTWARE;
-            } else if (strcasecmp(optarg, "sw") == 0) {
-                driver = DRIVER_SOFTWARE;
             } else if (strcasecmp(optarg, "ref") == 0) {
                 driver = DRIVER_REFERENCE;
             } else if (strcasecmp(optarg, "null") == 0) {