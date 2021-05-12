a4aba4e02d05bb64ac7835e3c1cf20b6b3ee7cfc
Jose Fonseca <jfonseca@vmware.com>
2021-05-12 20:37:01 +0100

    dispatch: Fork glproc_{gl,egl}.cpp for tracing/retracing.
    These functions need to do quite different things when tracing vs
    retracing.  In particular there are a lot of awkard things done when
    tracing, that simply get in the way when retracing.

---
+++ wrappers/glproc_egl.cpp
@@ -0,0 +1,188 @@
+/**************************************************************************
+ *
+ * Copyright 2011 Jose Fonseca
+ * All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ **************************************************************************/
+
+
+#include "glproc.hpp"
+
+
+#if !defined(_WIN32)
+#include "dlopen.hpp"
+#endif
+
+
+/*
+ * Handle to the true OpenGL library.
+ * XXX: Not really used yet.
+ */
+#if defined(_WIN32)
+HMODULE _libGlHandle = NULL;
+#else
+void *_libGlHandle = NULL;
+#endif
+
+
+
+#if defined(_WIN32)
+
+#error Unsupported
+
+#elif defined(__APPLE__)
+
+#error Unsupported
+
+#else
+
+#ifndef RTLD_DEEPBIND
+#define RTLD_DEEPBIND 0
+#endif
+
+/*
+ * Lookup a public EGL/GL/GLES symbol
+ *
+ * The spec states that eglGetProcAddress should only be used for non-core
+ * (extensions) entry-points.  Core entry-points should be taken directly from
+ * the API specific libraries.
+ *
+ */
+void *
+_getPublicProcAddress(const char *procName)
+{
+    void *proc;
+
+    /*
+     * We rely on dlsym(RTLD_NEXT, ...) whenever we can, because certain gl*
+     * symbols are exported by multiple APIs/SOs, and it's not trivial to
+     * determine which API/SO we should get the current symbol from.
+     */
+    proc = dlsym(RTLD_NEXT, procName);
+    if (proc) {
+        return proc;
+    }
+
+    /*
+     * dlsym(RTLD_NEXT, ...) will fail when the SO containing the symbol was
+     * loaded with RTLD_LOCAL.  We try to override RTLD_LOCAL with RTLD_GLOBAL
+     * when tracing but this doesn't always work.  So we try to guess and load
+     * the shared object directly.
+     *
+     * See https://github.com/apitrace/apitrace/issues/349#issuecomment-113316990
+     */
+
+    if (procName[0] == 'e' && procName[1] == 'g' && procName[2] == 'l') {
+        static void *libEGL = NULL;
+        if (!libEGL) {
+            libEGL = _dlopen("libEGL.so", RTLD_LOCAL | RTLD_LAZY | RTLD_DEEPBIND);
+            if (!libEGL) {
+                return NULL;
+            }
+        }
+        return dlsym(libEGL, procName);
+    }
+
+    /*
+     * This might happen when:
+     *
+     * - the application is querying non-extensions functions via
+     *   eglGetProcAddress (either because EGL_KHR_get_all_proc_addresses
+     *   is advertised, or merely because the EGL implementation supports
+     *   it regardless, like Mesa does)
+     *
+     * - libGLES*.so nor libGL*.so was ever loaded.
+     *
+     * - we need to resolve entrypoints that application never asked (e.g.,
+     *   glGetIntegerv), for internal purposes
+     *
+     * Therefore, we try to fallback to eglGetProcAddress.
+     *
+     * See https://github.com/apitrace/apitrace/issues/301#issuecomment-68532248
+     */
+    if (strcmp(procName, "eglGetProcAddress") != 0) {
+        proc = (void *) _eglGetProcAddress(procName);
+        if (proc) {
+            return proc;
+        }
+    }
+
+    /*
+     * TODO: We could futher mitigate against using the wrong SO by:
+     * - using RTLD_NOLOAD to ensure we only use an existing SO
+     * - the determine the right SO via eglQueryAPI and glGetString(GL_VERSION)
+     */
+
+    if (procName[0] == 'g' && procName[1] == 'l') {
+        /* TODO: Use GLESv1/GLESv2 on a per-context basis. */
+
+        static void *libGLESv2 = NULL;
+        if (!libGLESv2) {
+            libGLESv2 = _dlopen("libGLESv2.so", RTLD_LOCAL | RTLD_LAZY | RTLD_DEEPBIND);
+        }
+        if (libGLESv2) {
+            proc = dlsym(libGLESv2, procName);
+        }
+        if (proc) {
+            return proc;
+        }
+
+        static void *libGLESv1 = NULL;
+        if (!libGLESv1) {
+            libGLESv1 = _dlopen("libGLESv1_CM.so", RTLD_LOCAL | RTLD_LAZY | RTLD_DEEPBIND);
+        }
+        if (libGLESv1) {
+            proc = dlsym(libGLESv1, procName);
+        }
+        if (proc) {
+            return proc;
+        }
+    }
+
+    return NULL;
+}
+
+/*
+ * Lookup a private EGL/GL/GLES symbol
+ *
+ * Private symbols should always be available through eglGetProcAddress, and
+ * they are guaranteed to work with any context bound (regardless of the API).
+ *
+ * However, per issue#57, eglGetProcAddress returns garbage on some
+ * implementations, and the spec states that implementations may choose to also
+ * export extension functions publicly, so we always attempt dlsym before
+ * eglGetProcAddress to mitigate that.
+ */
+void *
+_getPrivateProcAddress(const char *procName)
+{
+    void *proc;
+    proc = _getPublicProcAddress(procName);
+    if (!proc &&
+        ((procName[0] == 'e' && procName[1] == 'g' && procName[2] == 'l') ||
+         (procName[0] == 'g' && procName[1] == 'l'))) {
+        proc = (void *) _eglGetProcAddress(procName);
+    }
+
+    return proc;
+}
+
+#endif
---
+++ wrappers/glproc_gl.cpp
@@ -0,0 +1,235 @@
+/**************************************************************************
+ *
+ * Copyright 2011 Jose Fonseca
+ * All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ **************************************************************************/
+
+
+#include "glproc.hpp"
+#include "os.hpp"
+#include "os_string.hpp"
+
+
+#if !defined(_WIN32)
+#include <unistd.h> // for symlink
+#include "dlopen.hpp"
+#endif
+
+
+/*
+ * Handle to the true OpenGL library.
+ */
+#if defined(_WIN32)
+HMODULE _libGlHandle = NULL;
+#else
+void *_libGlHandle = NULL;
+#endif
+
+
+
+#if defined(_WIN32)
+
+void *
+_getPublicProcAddress(const char *procName)
+{
+    if (!_libGlHandle) {
+        char szDll[MAX_PATH] = {0};
+        
+        if (!GetSystemDirectoryA(szDll, MAX_PATH)) {
+            return NULL;
+        }
+        
+        strcat(szDll, "\\opengl32.dll");
+        
+        _libGlHandle = LoadLibraryA(szDll);
+        if (!_libGlHandle) {
+            os::log("apitrace: error: couldn't load %s\n", szDll);
+            return NULL;
+        }
+    }
+        
+    return (void *)GetProcAddress(_libGlHandle, procName);
+}
+
+
+void *
+_getPrivateProcAddress(const char *procName) {
+    return (void *)_wglGetProcAddress(procName);
+}
+
+
+#elif defined(__APPLE__)
+
+
+/*
+ * Path to the true OpenGL framework
+ */
+static const char *libgl_filename = "/System/Library/Frameworks/OpenGL.framework/OpenGL";
+
+
+/*
+ * Lookup a libGL symbol
+ */
+void * _libgl_sym(const char *symbol)
+{
+    void *result;
+
+    if (!_libGlHandle) {
+        /* 
+         * Unfortunately we can't just dlopen the true dynamic library because
+         * DYLD_LIBRARY_PATH/DYLD_FRAMEWORK_PATH take precedence, even for
+         * absolute paths.  So we create a temporary symlink, and dlopen that
+         * instead.
+         */
+
+        os::String temp_template = os::getTemporaryDirectoryPath();
+        temp_template.append("tmp.XXXXXX");
+        char *temp_filename = temp_template.buf(temp_template.length() + 1);
+
+        if (mktemp(temp_filename) != NULL) {
+            if (symlink(libgl_filename, temp_filename) == 0) {
+                _libGlHandle = dlopen(temp_filename, RTLD_LOCAL | RTLD_NOW | RTLD_FIRST);
+                remove(temp_filename);
+            }
+        }
+
+        if (!_libGlHandle) {
+            os::log("apitrace: error: couldn't load %s\n", libgl_filename);
+            os::abort();
+            return NULL;
+        }
+    }
+
+    result = dlsym(_libGlHandle, symbol);
+
+#if 0
+    if (result && result == dlsym(RTLD_SELF, symbol)) {
+        os::log("apitrace: error: symbol lookup recursion\n");
+        os::abort();
+        return NULL;
+    }
+#endif
+
+    return result;
+}
+
+
+void *
+_getPublicProcAddress(const char *procName)
+{
+    return _libgl_sym(procName);
+}
+
+void *
+_getPrivateProcAddress(const char *procName)
+{
+    return _libgl_sym(procName);
+}
+
+
+#else
+
+#ifndef RTLD_DEEPBIND
+#define RTLD_DEEPBIND 0
+#endif
+
+static inline void
+logSymbol(const char *name, void *ptr) {
+    if (0) {
+        if (ptr) {
+            Dl_info info;
+            if (ptr && dladdr(ptr, &info)) {
+                os::log("apitrace: %s -> \"%s\"\n", name, info.dli_fname);
+            }
+        } else {
+            os::log("apitrace: %s -> NULL\n", name);
+        }
+    }
+}
+
+
+/*
+ * Lookup a libGL symbol
+ */
+void * _libgl_sym(const char *symbol)
+{
+    void *result;
+
+    if (!_libGlHandle) {
+        /*
+         * The app doesn't directly link against libGL.so, nor does it directly
+         * dlopen it.  So we have to load it ourselves.
+         */
+
+        const char * libgl_filename = getenv("TRACE_LIBGL");
+
+        if (!libgl_filename) {
+            /*
+             * Try to use whatever libGL.so the library is linked against.
+             */
+
+            result = dlsym(RTLD_NEXT, symbol);
+            if (result) {
+                _libGlHandle = RTLD_NEXT;
+                return result;
+            }
+
+            libgl_filename = "libGL.so.1";
+        }
+
+        /*
+         * It would have been preferable to use RTLD_LOCAL to ensure that the
+         * application can never access libGL.so symbols directly, but this
+         * won't work, given libGL.so often loads a driver specific SO and
+         * exposes symbols to it.
+         */
+
+        _libGlHandle = _dlopen(libgl_filename, RTLD_GLOBAL | RTLD_LAZY | RTLD_DEEPBIND);
+        if (!_libGlHandle) {
+            os::log("apitrace: error: couldn't find libGL.so\n");
+            return NULL;
+        }
+    }
+
+    result = dlsym(_libGlHandle, symbol);
+
+    logSymbol(symbol, result);
+
+    return result;
+}
+
+
+void *
+_getPublicProcAddress(const char *procName)
+{
+    return _libgl_sym(procName);
+}
+
+void *
+_getPrivateProcAddress(const char *procName)
+{
+    return (void *)_glXGetProcAddressARB((const GLubyte *)procName);
+}
+
+
+#endif 
+
