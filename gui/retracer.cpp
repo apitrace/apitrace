899f83f09bbcd376deb818a37687d016a9661625
Chris Djali <christopher.djali@wrld3d.com>
2022-03-30 19:38:45 +0100

    Prevent buffer underrun wehn readLine reads nothing
    Zero-length reads return -1, which decreased the size, making it negative.

--- gui/retracer.cpp
+++ gui/retracer.cpp
@@ -510,13 +510,15 @@ void Retracer::run()
 
                 for (int headerLine = 0; headerLine < headerLines; ++headerLine) {
                     qint64 headerRead = io.readLine(&header[headerSize], sizeof(header) - headerSize);
+                    if (headerRead > 0)
+                    {
+                        // if header actually contains optional comment line, ...
+                        if (headerLine == 1 && header[headerSize] == '#') {
+                            ++headerLines;
+                        }
 
-                    // if header actually contains optional comment line, ...
-                    if (headerLine == 1 && header[headerSize] == '#') {
-                        ++headerLines;
+                        headerSize += headerRead;
                     }
-
-                    headerSize += headerRead;
                 }
 
                 const char *headerEnd = image::readPNMHeader(header, headerSize, info);
