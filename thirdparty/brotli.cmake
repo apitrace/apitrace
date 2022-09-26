72315af8e7b08b99c4ff914eed3a9ad21d70f3c0
Jose Fonseca <jfonseca@vmware.com>
2022-09-26 13:11:34 +0100

    brotli: Upgrade to v1.0.7.

--- thirdparty/brotli.cmake
+++ thirdparty/brotli.cmake
@@ -7,7 +7,11 @@ elseif (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 endif ()
 
 add_convenience_library(brotli_common STATIC EXCLUDE_FROM_ALL
+    brotli/c/common/constants.c
+    brotli/c/common/context.c
     brotli/c/common/dictionary.c
+    brotli/c/common/platform.c
+    brotli/c/common/shared_dictionary.c
     brotli/c/common/transform.c
 )
 
@@ -29,12 +33,15 @@ add_convenience_library (brotli_enc STATIC EXCLUDE_FROM_ALL
     brotli/c/enc/block_splitter.c
     brotli/c/enc/brotli_bit_stream.c
     brotli/c/enc/cluster.c
+    brotli/c/enc/command.c
+    brotli/c/enc/compound_dictionary.c
     brotli/c/enc/compress_fragment.c
     brotli/c/enc/compress_fragment_two_pass.c
     brotli/c/enc/dictionary_hash.c
     brotli/c/enc/encode.c
     brotli/c/enc/encoder_dict.c
     brotli/c/enc/entropy_encode.c
+    brotli/c/enc/fast_log.c
     brotli/c/enc/histogram.c
     brotli/c/enc/literal_cost.c
     brotli/c/enc/memory.c
