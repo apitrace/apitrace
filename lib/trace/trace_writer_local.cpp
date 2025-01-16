apitrace/apitrace

commit 4d15c6dde5e79757f108de7954d218334ea439df

Don't make timestamp and output options mutually exclusive
The timestamp is inserted right before the extension if there is one
in the output filename provided by the user.

Signed-off-by: Louis-Francis Ratté-Boulianne <lfrb@collabora.com>

lib/trace/trace_writer_local.cpp
@@ -108,11 +108,18 @@ LocalWriter::open(void) {
     os::String szFileName;
 
     const char *lpFileName;
+    char suffix[17] = "";
+    bool hasTimestamp;
+
+    hasTimestamp = boolOption(getenv("TRACE_TIMESTAMP"), false);
+    if (hasTimestamp) {
+        std::time_t time = std::time({});
+        strftime(suffix, sizeof(suffix), ".%Y%m%dT%H%M%S", std::localtime(&time));
+    }
 
     lpFileName = getenv("TRACE_FILE");
     if (!lpFileName) {
         static unsigned dwCounter = 0;
-        char suffix[17] = "";
 
         os::String process = os::getProcessName();
 #ifdef _WIN32
@@ -132,12 +139,6 @@ LocalWriter::open(void) {
 #endif
         prefix.join(process);
 
-        const char *lpTimestamp = getenv("TRACE_TIMESTAMP");
-        if (lpTimestamp && boolOption(lpTimestamp)) {
-            std::time_t time = std::time({});
-            strftime(suffix, sizeof(suffix), ".%Y%m%dT%H%M%S", std::localtime(&time));
-        }
-
         for (;;) {
             FILE *file;
 
@@ -155,6 +156,12 @@ LocalWriter::open(void) {
 
             ++dwCounter;
         }
+    } else if (hasTimestamp) {
+        const char *ext = strrchr(lpFileName, '.');
+        size_t length = ext ? ext - lpFileName : strlen(lpFileName);
+
+        szFileName = os::String::format("%.*s%s%s", length, lpFileName, suffix, ext ? ext : "");
+        lpFileName = szFileName;
     }
 
     os::log("apitrace: tracing to %s\n", lpFileName);

cli/cli_trace.cpp
@@ -269,7 +269,8 @@ traceProgram(trace::API api,
 
         if (output) {
             os::setEnvironment("TRACE_FILE", output);
-        } else if (timestamp) {
+        }
+        if (timestamp) {
             os::setEnvironment("TRACE_TIMESTAMP", "1");
         }
 
@@ -348,8 +349,7 @@ usage(void)
         "                        default is `gl`\n"
         "    -o, --output=TRACE  specify output trace file;\n"
         "                        default is `PROGRAM.trace`\n"
-        "    -t, --timestamp     append timestamp to output trace filename;\n"
-        "                        ignored if --output argument is specified\n"
+        "    -t, --timestamp     append timestamp to output trace filename\n"
 #ifdef TRACE_VARIABLE
         "    -d,  --debug        run inside debugger (gdb/lldb)\n"
 #endif

commit 5be0ca4b2ff38bca13238622777e897d2d7f1d69

trace: Fix format warning
Fix the following format warning by changing the length type from size_t
to int:

lib/trace/trace_writer_local.cpp: In member function ‘void trace::LocalWriter::open()’:
lib/trace/trace_writer_local.cpp:163:44: warning: field precision specifier ‘.*’ expects argument of type ‘int’, but argument 2 has type ‘size_t’ {aka ‘long unsigned int’} [-Wformat=]
  163 |         szFileName = os::String::format("%.*s%s%s", length, lpFileName, suffix, ext ? ext : "");
      |                                          ~~^~       ~~~~~~
      |                                            |        |
      |                                            int      size_t {aka long unsigned int}

Fixes: 4d15c6dde5e7 ("Don't make timestamp and output options mutually exclusive")
Signed-off-by: James Hogan <james@albanarts.com>

lib/trace/trace_writer_local.cpp
@@ -158,7 +158,7 @@ LocalWriter::open(void) {
         }
     } else if (hasTimestamp) {
         const char *ext = strrchr(lpFileName, '.');
-        size_t length = ext ? ext - lpFileName : strlen(lpFileName);
+        int length = ext ? ext - lpFileName : strlen(lpFileName);
 
         szFileName = os::String::format("%.*s%s%s", length, lpFileName, suffix, ext ? ext : "");
         lpFileName = szFileName;