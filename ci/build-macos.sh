2c8f1ec5176a818f11d9261af841b6424861a25f
Jose Fonseca <jfonseca@vmware.com>
2025-01-16 11:18:07 +0000

    ci: Upgrade to latest macos image.
    As macos-12 image is gone.  This implies abandoning building the Qt GUI,
    as there's not suitable Qt5 binaries around.

--- ci/build-macos.sh
+++ ci/build-macos.sh
@@ -2,30 +2,7 @@
 
 set -eu
 
-qtVersion=5.15.2
-qtTag=0-202011130601
-qtVersionDotless=$(echo $qtVersion | sed -e 's/\.//g')
-qtArchive=$qtVersion-${qtTag}qtbase-MacOS-MacOS_10_13-Clang-MacOS-MacOS_10_13-X86_64.7z
-qtArchiveUrl=https://download.qt.io/online/qtsdkrepository/mac_x64/desktop/qt5_$qtVersionDotless/qt.qt5.$qtVersionDotless.clang_64/$qtArchive
-
 set -x
-
-mkdir -p qt
-pushd qt
-
-if [ ! -e $qtArchive ]
-then
-    curl -sLO $qtArchiveUrl
-fi
-
-qtToolchainPath=$PWD/$qtVersion/clang_64
-if [ ! -d $qtToolchainPath ]
-then
-    cmake -E tar xf $qtArchive --format=7zip
-    test -d $qtToolchainPath
-fi
-
-popd
 
 if which ninja > /dev/null
 then
@@ -37,6 +14,6 @@ fi
 export CMAKE_BUILD_PARALLEL_LEVEL=$(getconf _NPROCESSORS_ONLN)
 
 
-cmake -S. -Bbuild -G "$cmakeGenerator" -DCMAKE_PREFIX_PATH=$qtToolchainPath -DENABLE_GUI=ON
+cmake -S. -Bbuild -G "$cmakeGenerator" -DENABLE_GUI=OFF
 cmake --build build --use-stderr
 cmake --build build --use-stderr --target check
