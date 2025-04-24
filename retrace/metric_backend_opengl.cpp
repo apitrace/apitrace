apitrace/apitrace

commit 1937ee8eb5e5ef816481b283ec9850f62132b043

retrace: Use right API calls when using EXT_disjoint_timer_query
Even though the Mesa drivers are aliasing the EXT API calls with the unsuffixed
versions, it doesn't work with all drivers (e.g. libmali). Selecting the
right call to use depending on the supported extensions makes it work
universally.

Signed-off-by: Louis-Francis Ratté-Boulianne <lfrb@collabora.com>

retrace/metric_backend_opengl.cpp
@@ -152,6 +152,32 @@ int64_t MetricBackend_opengl::getTimeFrequency(void) {
     }
 }
 
+void MetricBackend_opengl::queryTimestamp(GLuint query) {
+    if (supportsDisjoint) {
+        glQueryCounterEXT(query, GL_TIMESTAMP_EXT);
+    } else {
+        glQueryCounter(query, GL_TIMESTAMP);
+    }
+}
+
+int64_t MetricBackend_opengl::getQueryResult(GLuint query) {
+    int64_t result;
+
+    if (supportsDisjoint) {
+        glGetQueryObjecti64vEXT(query, GL_QUERY_RESULT_EXT, &result);
+    } else if (supportsTimestamp) {
+        glGetQueryObjecti64v(query, GL_QUERY_RESULT, &result);
+    } else if (supportsElapsed) {
+        glGetQueryObjecti64vEXT(query, GL_QUERY_RESULT, &result);
+    } else {
+        uint32_t result32;
+        glGetQueryObjectuiv(query, GL_QUERY_RESULT, &result32);
+        result = static_cast<int64_t>(result32);
+    }
+
+    return result;
+}
+
 
 bool MetricBackend_opengl::isSupported() {
     return true;
@@ -332,8 +358,7 @@ void MetricBackend_opengl::processQueries() {
         while (!queries[i].empty()) {
             auto &query = queries[i].front();
             if (metrics[METRIC_GPU_START].profiled[i]) {
-                glGetQueryObjecti64v(query[QUERY_GPU_START], GL_QUERY_RESULT,
-                                      &timestamp);
+                timestamp = getQueryResult(query[QUERY_GPU_START]);
                 if (timestamp < gpuStart && timestamp_bits_precision < 64) {
                     // Correct clock domain of baseTime after roll-over
                     baseTime -= (UINT64_C(2) << timestamp_bits_precision) - 1;
@@ -345,32 +370,21 @@ void MetricBackend_opengl::processQueries() {
             if (metrics[METRIC_GPU_DURATION].profiled[i]) {
                 if (supportsTimestamp) {
                     if (!metrics[METRIC_GPU_START].profiled[i]) {
-                        glGetQueryObjecti64v(query[QUERY_GPU_START], GL_QUERY_RESULT,
-                                             &gpuStart);
+                        gpuStart = getQueryResult(query[QUERY_GPU_START]);
                     }
-                    glGetQueryObjecti64v(query[QUERY_GPU_DURATION], GL_QUERY_RESULT,
-                                         &gpuEnd);
+                    gpuEnd = getQueryResult(query[QUERY_GPU_START]);
                     if (gpuEnd < gpuStart && timestamp_bits_precision < 64) {
                         // Correct the roll-over
                         gpuEnd += (UINT64_C(2) << timestamp_bits_precision) - 1;
                     }
                     gpuEnd -= gpuStart;
                 } else {
-                    glGetQueryObjecti64vEXT(query[QUERY_GPU_DURATION], GL_QUERY_RESULT,
-                                            &gpuEnd);
+                    gpuEnd = getQueryResult(query[QUERY_GPU_DURATION]);
                 }
                 data[METRIC_GPU_DURATION][i]->addData(boundary, gpuEnd);
             }
             if (metrics[METRIC_GPU_PIXELS].profiled[i]) {
-                if (supportsTimestamp) {
-                    glGetQueryObjecti64v(query[QUERY_OCCLUSION], GL_QUERY_RESULT, &pixels);
-                } else if (supportsElapsed) {
-                    glGetQueryObjecti64vEXT(query[QUERY_OCCLUSION], GL_QUERY_RESULT, &pixels);
-                } else {
-                    uint32_t pixels32;
-                    glGetQueryObjectuiv(query[QUERY_OCCLUSION], GL_QUERY_RESULT, &pixels32);
-                    pixels = static_cast<int64_t>(pixels32);
-                }
+                pixels = getQueryResult(query[QUERY_OCCLUSION]);
                 data[METRIC_GPU_PIXELS][i]->addData(boundary, pixels);
             }
             glDeleteQueries(QUERY_LIST_END, query.data());
@@ -409,7 +423,7 @@ void MetricBackend_opengl::beginQuery(QueryBoundary boundary) {
                 curDisjoint += disjointOccurred;
                 disjointStart[boundary] = curDisjoint;
             }
-            glQueryCounter(query[QUERY_GPU_START], GL_TIMESTAMP);
+            queryTimestamp(query[QUERY_GPU_START]);
         }
         if (metrics[METRIC_GPU_DURATION].profiled[boundary] && !supportsTimestamp) {
             glBeginQuery(GL_TIME_ELAPSED, query[QUERY_GPU_DURATION]);
@@ -481,7 +495,7 @@ void MetricBackend_opengl::endQuery(QueryBoundary boundary) {
             if (metrics[METRIC_GPU_DURATION].profiled[boundary] && supportsTimestamp) {
                 // GL_TIME_ELAPSED cannot be used in nested queries
                 // so prefer this if timestamps are supported
-                glQueryCounter(query[QUERY_GPU_DURATION], GL_TIMESTAMP);
+                queryTimestamp(query[QUERY_GPU_DURATION]);
                 if (metrics[METRIC_GPU_DISJOINT].profiled[boundary]) {
                     GLint disjointOccurred = 0;
                     glGetIntegerv(GL_GPU_DISJOINT_EXT, &disjointOccurred);

retrace/glretrace_main.cpp
@@ -205,6 +205,34 @@ getTimeFrequency(void) {
 }
 
 static inline void
+queryTimestamp(GLuint query) {
+    if (supportsDisjoint) {
+        glQueryCounterEXT(query, GL_TIMESTAMP_EXT);
+    } else {
+        glQueryCounter(query, GL_TIMESTAMP);
+    }
+}
+
+static inline int64_t
+getQueryResult(GLuint query) {
+    int64_t result = 0;
+
+    if (supportsDisjoint) {
+        glGetQueryObjecti64vEXT(query, GL_QUERY_RESULT_EXT, &result);
+    } else if (supportsTimestamp) {
+        glGetQueryObjecti64v(query, GL_QUERY_RESULT, &result);
+    } else if (supportsElapsed) {
+        glGetQueryObjecti64vEXT(query, GL_QUERY_RESULT, &result);
+    } else {
+        uint32_t result32;
+        glGetQueryObjectuiv(query, GL_QUERY_RESULT, &result32);
+        result = static_cast<int64_t>(result32);
+    }
+
+    return result;
+}
+
+static inline void
 getCurrentVsize(int64_t& vsize) {
     vsize = os::getVsize();
 }
@@ -224,23 +252,15 @@ completeCallQuery(CallQuery& query) {
         if (retrace::profilingGpuTimes) {
             if (supportsTimestamp) {
                 /* Use ARB queries in case EXT not present */
-                glGetQueryObjecti64v(query.ids[GPU_START], GL_QUERY_RESULT, &gpuStart);
-                glGetQueryObjecti64v(query.ids[GPU_DURATION], GL_QUERY_RESULT, &gpuDuration);
+                gpuStart = getQueryResult(query.ids[GPU_START]);
+                gpuDuration = getQueryResult(query.ids[GPU_DURATION]);
             } else {
-                glGetQueryObjecti64vEXT(query.ids[GPU_DURATION], GL_QUERY_RESULT, &gpuDuration);
+                gpuDuration = getQueryResult(query.ids[GPU_DURATION]);
             }
         }
 
         if (retrace::profilingPixelsDrawn) {
-            if (supportsTimestamp) {
-                glGetQueryObjecti64v(query.ids[OCCLUSION], GL_QUERY_RESULT, &pixels);
-            } else if (supportsElapsed) {
-                glGetQueryObjecti64vEXT(query.ids[OCCLUSION], GL_QUERY_RESULT, &pixels);
-            } else {
-                uint32_t pixels32;
-                glGetQueryObjectuiv(query.ids[OCCLUSION], GL_QUERY_RESULT, &pixels32);
-                pixels = static_cast<int64_t>(pixels32);
-            }
+            pixels = getQueryResult(query.ids[OCCLUSION]);
         }
 
     } else {
@@ -315,10 +335,7 @@ beginProfile(trace::Call &call, bool isDraw) {
     /* GPU profiling only for draw calls */
     if (isDraw) {
         if (retrace::profilingGpuTimes) {
-            if (supportsTimestamp) {
-                glQueryCounter(query.ids[GPU_START], GL_TIMESTAMP);
-            }
-
+            queryTimestamp(query.ids[GPU_START]);
             glBeginQuery(GL_TIME_ELAPSED, query.ids[GPU_DURATION]);
         }
 

retrace/metric_backend_opengl.hpp
@@ -198,6 +198,10 @@ private:
 
     int64_t getTimeFrequency(void);
 
+    void queryTimestamp(GLuint query);
+
+    int64_t getQueryResult(GLuint query);
+
     void processQueries();
 };
 

commit 790e8cb77e773672a2c0a99c0710fcff16b85b6e

retrace: Fix GPU Duration calculation
After the API calls refactoring, we got a typo in GPU duration, which
basically made all measurements became zero-valued, since it was
subtraction GPU start timestamp with itself.

Fixes: 1937ee8eb5e5ef816481b283ec9850f62132b043
Signed-off-by: Guilherme Gallo <guilherme.gallo@collabora.com>

retrace/metric_backend_opengl.cpp
@@ -372,7 +372,7 @@ void MetricBackend_opengl::processQueries() {
                     if (!metrics[METRIC_GPU_START].profiled[i]) {
                         gpuStart = getQueryResult(query[QUERY_GPU_START]);
                     }
-                    gpuEnd = getQueryResult(query[QUERY_GPU_START]);
+                    gpuEnd = getQueryResult(query[QUERY_GPU_DURATION]);
                     if (gpuEnd < gpuStart && timestamp_bits_precision < 64) {
                         // Correct the roll-over
                         gpuEnd += (UINT64_C(2) << timestamp_bits_precision) - 1;